44149cb84ac4888809c26e74879eceb8782849dd
James D. Callahan III <jcallahan@curse.com>
2016-09-07 08:25:09 -0500

    Somehow itIT wasn't ever added. Fixed.

---
+++ Localization/itIT.lua
@@ -0,0 +1,10 @@
+local AddOnFolderName, private = ...
+local L = _G.LibStub("AceLocale-3.0"):NewLocale(AddOnFolderName, "itIT")
+if not L then return end
+
+--@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="comment")@
+
+local VL = _G.LibStub("AceLocale-3.0"):NewLocale(AddOnFolderName .. "Vignette", "itIT")
+if not VL then return end
+
+--@localization(locale="itIT", namespace="Vignette", table-name="VL", format="lua_additive_table", handle-unlocalized="comment")@
