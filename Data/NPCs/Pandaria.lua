35ac1d2cab0012bd94b03127b925d036e522d7f6
James D. Callahan III <jcallahan@curse.com>
2018-07-07 10:48:26 -0500

    Add tracking quests and vignette names for Timeless Isles NPCs.

--- Data/NPCs/Pandaria.lua
+++ Data/NPCs/Pandaria.lua
@@ -68,26 +68,128 @@ NPCs[69998] = { -- Goda
 -- ----------------------------------------------------------------------------
 -- Timeless Isle (951)
 -- ----------------------------------------------------------------------------
+NPCs[71919] = { -- Zhu-Gon the Sour
+	questID = 32959, -- Tracking Quest - Daily - Zhu-Gon the Sour/Skunky Beer
+	vignetteName = "Really Skunky Beer"
+}
+
+NPCs[72045] = { -- Chelon
+	questID = 32966, -- Tracking Quest - Daily - Chelon
+}
+
 NPCs[72048] = { -- Rattleskew
 	vignetteName = "Battle of the Barnacle",
 }
 
+NPCs[72049] = { -- Cranegnasher
+	questID = 32967, -- Tracking Quest - Daily - Cranegnasher
+}
+
+NPCs[72193] = { -- Karkanos
+	questID = 33292, -- Tracking Quest - Daily - Karkanos
+}
+
+NPCs[72245] = { -- Zesqua
+	questID = 33316, -- Tracking Quest - Daily - Zesqua
+}
+
+NPCs[72769] = { -- Spirit of Jadefire
+	questID = 33293, -- Tracking Quest - Daily - Spirit of Jadefire
+}
+
+NPCs[72775] = { -- Bufo
+	questID = 33301, -- Tracking Quest - Daily - Bufo
+}
+
 NPCs[72808] = { -- Tsavo'ka
 	isTameable = true,
+	questID = 33304, -- Tracking Quest - Daily - Tsavo'ka
+}
+
+NPCs[72909] = { -- Gu'chi the Swarmbringer
+	questID = 33294, -- Tracking Quest - Daily - Gu'chi the Swarmbringer
+}
+
+NPCs[72970] = { -- Golganarr
+	questID = 33315, -- Tracking Quest - Daily - Golganarr
+}
+
+NPCs[73157] = { -- Rock Moss
+	questID = 33307, -- Tracking Quest - Daily - Rock Moss
 }
 
 NPCs[73158] = { -- Emerald Gander
 	isTameable = true,
+	questID = 33295, -- Tracking Quest - Daily - Emerald Gander
 }
 
 NPCs[73160] = { -- Ironfur Steelhorn
 	isTameable = true,
+	questID = 33296, -- Tracking Quest - Daily - Ironfur Steelhorn
 }
 
 NPCs[73161] = { -- Great Turtle Furyshell
 	isTameable = true,
+	questID = 33297, -- Tracking Quest - Daily - Great Turtle Furyshell
+}
+
+NPCs[73163] = { -- Imperial Python
+	questID = 33303, -- Tracking Quest - Daily - Imperial Python
 }
 
 NPCs[73166] = { -- Monstrous Spineclaw
 	isTameable = true,
+	questID = 33302, -- Tracking Quest - Daily - Monstrous Spineclaw
+}
+
+NPCs[73167] = { -- Huolon
+	questID = 33311, -- Tracking Quest - Daily - Huolon
+}
+
+NPCs[73169] = { -- Jakur of Ordon
+	questID = 33306, -- Tracking Quest - Daily - Jakur of Ordon
+}
+
+NPCs[73170] = { -- Watcher Osu
+	questID = 33322, -- Tracking Quest - Daily - Watcher Osu
+}
+
+NPCs[73171] = { -- Champion of the Black Flame
+	questID = 33299, -- Tracking Quest - Daily - Champion of the Black Flame
+}
+
+NPCs[73172] = { -- Flintlord Gairan
+	questID = 33309, -- Tracking Quest - Daily - Flintlord Gairan
+}
+
+NPCs[73173] = { -- Urdur the Cauterizer
+	questID = 33308, -- Tracking Quest - Daily - Urdur the Cauterizer
+}
+
+NPCs[73175] = { -- Cinderfall
+	questID = 33310, -- Tracking Quest - Daily - Cinderfall
+}
+
+NPCs[73277] = { -- Leafmender
+	questID = 33298, -- Tracking Quest - Daily - Leafmender
+}
+
+NPCs[73279] = { -- Evermaw
+	questID = 33313, -- Tracking Quest - Daily - Evermaw
+}
+
+NPCs[73281] = { -- Dread Ship Vazuvius
+	questID = 33314, -- Tracking Quest - Daily - Dread Ship Vazuvius
+}
+
+NPCs[73282] = { -- Garnia
+	questID = 33300, -- Tracking Quest - Daily - Garnia
+}
+
+NPCs[73666] = { -- Archiereus of Flame (Three-Breeze Terrace)
+	questID = 33312, -- Tracking Quest - Daily - Archiereus of Flame
+}
+
+NPCs[73704] = { -- Stinkbraid
+	questID = 33305, -- Tracking Quest - Daily - Stinkbraid
 }
