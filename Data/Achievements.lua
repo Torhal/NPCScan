0658f39f168c802b35f3e961bd9ee0c8f88598bf
James D. Callahan III <jcallahan@curse.com>
2018-07-18 13:46:45 -0500

    Move the AchievementID table into Achievements.lua

--- Data/Achievements.lua
+++ Data/Achievements.lua
@@ -7,9 +7,45 @@ local pairs = _G.pairs
 -- AddOn namespace
 -- ----------------------------------------------------------------------------
 local AddOnFolderName, private = ...
-local AchievementID = private.Enum.AchievementID
 local ContinentID = private.Enum.ContinentID
 local Data = private.Data
+
+local AchievementID = {
+	AdventurerOfAzsuna = 11261,
+	AdventurerOfHighmountain = 11264,
+	AdventurerOfStormheim = 11263,
+	AdventurerOfSuramar = 11265,
+	AdventurerOfValsharah = 11262,
+	AncientNoMore = 9678,
+	BloodyRare = 1312,
+	BrokeBackPrecipice = 9571,
+	ChampionsOfLeiShen = 8103,
+	CommanderOfArgus = 12078,
+	CutOffTheHead = 9633,
+	FightThePower = 9655,
+	Frostbitten = 2257,
+	Glorious = 7439,
+	GorgrondMonsterHunter = 9400,
+	Hellbane = 10061,
+	HeraldsOfTheLegion = 9638,
+	HighValueTargets = 9216,
+	ImInYourBaseKillingYourDudes = 7932,
+	JungleStalker = 10070,
+	KingOfTheMonsters = 9601,
+	MakingTheCut = 9617,
+	MillionsOfYearsOfEvolutionVsMyFist = 8123,
+	NaxtVictim = 11841,
+	OneManArmy = 7317,
+	PraiseTheSun = 8028,
+	Predator = 10334,
+	TerrorsOfTheShore = 11786,
+	TheSongOfSilence = 9541,
+	TimelessChampion = 8714,
+	UnleashedMonstrosities = 11160,
+	ZulAgain = 8078
+}
+
+private.Enum.AchievementID = AchievementID
 
 Data.Achievements = {
 	[AchievementID.AdventurerOfAzsuna] = {
