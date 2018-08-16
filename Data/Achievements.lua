7335a2c48da4b52c3f68da8293f74c2173ff01f1
James D. Callahan III <jcallahan@curse.com>
2018-08-16 03:42:45 -0500

    Refactor achievement initialization code: Remove all references to continentID, dynamically allocate tables based on defined achievement IDs, and then add default data via a separate table.

--- Data/Achievements.lua
+++ Data/Achievements.lua
@@ -7,7 +7,6 @@ local pairs = _G.pairs
 -- AddOn namespace
 -- ----------------------------------------------------------------------------
 local AddOnFolderName, private = ...
-local ContinentID = private.Enum.ContinentID
 local Data = private.Data
 
 local AchievementID = {
@@ -47,67 +46,20 @@ local AchievementID = {
 
 private.Enum.AchievementID = AchievementID
 
-Data.Achievements = {
-	[AchievementID.AdventurerOfAzsuna] = {
-		continentID = ContinentID.BrokenIsles
-	},
-	[AchievementID.AdventurerOfHighmountain] = {
-		continentID = ContinentID.BrokenIsles
-	},
-	[AchievementID.AdventurerOfStormheim] = {
-		continentID = ContinentID.BrokenIsles
-	},
-	[AchievementID.AdventurerOfSuramar] = {
-		continentID = ContinentID.BrokenIsles
-	},
-	[AchievementID.AdventurerOfValsharah] = {
-		continentID = ContinentID.BrokenIsles
-	},
+Data.Achievements = {}
+
+for _, achievementID in pairs(AchievementID) do
+	Data.Achievements[achievementID] = {}
+end
+
+local achievementDataDefaults = {
 	[AchievementID.AncientNoMore] = {
-		continentID = ContinentID.Draenor,
 		criteriaNPCs = {
 			[86258] = true, -- Nultra
 			[86259] = true, -- Valstil
 		},
 	},
-	[AchievementID.BloodyRare] = {
-		continentID = ContinentID.Outland
-	},
-	[AchievementID.BrokeBackPrecipice] = {
-		continentID = ContinentID.Draenor
-	},
-	[AchievementID.ChampionsOfLeiShen] = {
-		continentID = ContinentID.Pandaria
-	},
-	[AchievementID.CommanderOfArgus] = {
-		continentID = ContinentID.BrokenIsles
-	},
-	[AchievementID.CutOffTheHead] = {
-		continentID = ContinentID.Draenor
-	},
-	[AchievementID.FightThePower] = {
-		continentID = ContinentID.Draenor
-	},
-	[AchievementID.Frostbitten] = {
-		continentID = ContinentID.Northrend
-	},
-	[AchievementID.Glorious] = {
-		continentID = ContinentID.Pandaria
-	},
-	[AchievementID.GorgrondMonsterHunter] = {
-		continentID = ContinentID.Draenor
-	},
-	[AchievementID.Hellbane] = {
-		continentID = ContinentID.Draenor
-	},
-	[AchievementID.HeraldsOfTheLegion] = {
-		continentID = ContinentID.Draenor
-	},
-	[AchievementID.HighValueTargets] = {
-		continentID = ContinentID.Draenor
-	},
 	[AchievementID.ImInYourBaseKillingYourDudes] = {
-		continentID = ContinentID.Pandaria,
 		criteriaNPCs = {
 			[68317] = true, -- Mavis Harms
 			[68318] = true, -- Dalan Nightbreaker
@@ -117,50 +69,22 @@ Data.Achievements = {
 			[68322] = true, -- Muerta
 		},
 	},
-	[AchievementID.JungleStalker] = {
-		continentID = ContinentID.Draenor
-	},
-	[AchievementID.KingOfTheMonsters] = {
-		continentID = ContinentID.Draenor
-	},
-	[AchievementID.MakingTheCut] = {
-		continentID = ContinentID.Draenor
-	},
 	[AchievementID.MillionsOfYearsOfEvolutionVsMyFist] = {
-		continentID = ContinentID.Pandaria,
 		criteriaNPCs = {
 			[69161] = true, -- Oondasta
 		}
 	},
-	[AchievementID.NaxtVictim] = {
-		continentID = ContinentID.BrokenIsles
-	},
-	[AchievementID.OneManArmy] = {
-		continentID = ContinentID.Pandaria
-	},
 	[AchievementID.PraiseTheSun] = {
-		continentID = ContinentID.Pandaria,
 		criteriaNPCs = {
 			[69099] = true, -- Nalak¸
 		},
 	},
 	[AchievementID.Predator] = {
-		continentID = ContinentID.Draenor,
 		criteriaNPCs = {
 			[96235] = true, -- Xemirkol
 		}
 	},
-	[AchievementID.TerrorsOfTheShore] = {
-		continentID = ContinentID.BrokenIsles
-	},
-	[AchievementID.TheSongOfSilence] = {
-		continentID = ContinentID.Draenor
-	},
-	[AchievementID.TimelessChampion] = {
-		continentID = ContinentID.Pandaria
-	},
 	[AchievementID.UnleashedMonstrosities] = {
-		continentID = ContinentID.BrokenIsles,
 		criteriaNPCs = {
 			[106981] = true, -- Captain Hring
 			[106982] = true, -- Reaver Jdorn
@@ -168,7 +92,6 @@ Data.Achievements = {
 		},
 	},
 	[AchievementID.ZulAgain] = {
-		continentID = ContinentID.Pandaria,
 		criteriaNPCs = {
 			[69768] = true, -- Zandalari Warscout
 			[69769] = true, -- Zandalari Warbringer
@@ -177,6 +100,10 @@ Data.Achievements = {
 		},
 	},
 }
+
+for achievementID, data in pairs(achievementDataDefaults) do
+	Data.Achievements[achievementID] = data
+end
 
 -- ----------------------------------------------------------------------------
 -- Achievements.
