cff83c52a7ef41e788d1cfe9bed600ef18c736ae
James D. Callahan III <jcallahan@curse.com>
2018-08-19 04:45:30 -0500

    Fix several incorrect questIDs and erroneous factionGroup settings, add vignetteIDs where appropriate, and remove all World Quests save for a very few where that is the only time the NPC spawns - it was an ill-conceived idea that only served to have NPCs alert continually until said quest was back up and subsequently completed.

--- Data/NPCs/KulTiras.lua
+++ Data/NPCs/KulTiras.lua
@@ -9,35 +9,30 @@ local NPCs = private.Data.NPCs
 -- ----------------------------------------------------------------------------
 NPCs[124548] = { -- Betsy
 	achievementQuestID = 47884, -- Betsy
-	questID = 51469, -- Betsy
 }
 
 NPCs[125453] = { -- Quillrat Matriarch
 	achievementQuestID = 48178, -- Quillrat Matriarch
 	isTameable = true,
-	questID = 51505, -- Quillrat Matriarch
 }
 
 NPCs[126621] = { -- Bonesquall
 	achievementQuestID = 48978, -- Bonesquall
-	questID = 51468, -- Bonesquall
 }
 
 NPCs[127129] = { -- Grozgore
 	achievementQuestID = 49388, -- Grozgore
-	questID = 51909, -- Grozgore
 }
 
 NPCs[127333] = { -- Barbthorn Queen
 	achievementQuestID = 48842, -- Barbthorn Queen
 	isTameable = true,
-	questID = 51506, -- Barbthorn Queen
 }
 
 NPCs[127651] = { -- Vicemaul
 	achievementQuestID = 48928, -- Vicemaul
 	isTameable = true,
-	questID = 51508, -- Vicemaul
+	vignetteID = 2293, -- Untended Fishing Rod
 }
 
 NPCs[127844] = { -- Gluttonous Yeti
@@ -46,13 +41,11 @@ NPCs[127844] = { -- Gluttonous Yeti
 
 NPCs[127877] = { -- Longfang
 	achievementQuestID = 48981, -- Longfang
-	factionGroup = "Alliance",
 	vignetteID = 2311, -- Longfang & Breakwater
 }
 
 NPCs[127901] = { -- Henry Breakwater
 	achievementQuestID = 48981, -- Longfang
-	factionGroup = "Alliance",
 	vignetteID = 2311, -- Longfang & Breakwater
 }
 
@@ -63,12 +56,10 @@ NPCs[128181] = { -- Vengeful Dead
 
 NPCs[128707] = { -- Rimestone
 	achievementQuestID = 49269, -- Rimestone
-	questID = 51897, -- Rimestone
 }
 
 NPCs[128973] = { -- Whargarble the Ill-Tempered
 	achievementQuestID = 49311, -- Whargarble the Ill-Tempered
-	questID = 51457, -- Whargarble the Ill-Tempered
 }
 
 NPCs[129031] = { -- Manifested Hate
@@ -78,148 +69,127 @@ NPCs[129031] = { -- Manifested Hate
 
 NPCs[129805] = { -- Beshol
 	achievementQuestID = 49481, -- Beshol,
-	questID = 51917, -- Beshol
 	vignetteID = 2420, -- Obviously Safe Chest
 }
 
 NPCs[129835] = { -- Gorehorn
 	achievementQuestID = 49480, -- Gorehorn
-	questID = 51507, -- Gorehorn
 }
 
 NPCs[129904] = { -- Cottontail Matron
 	achievementQuestID = 49216, -- Beastly Ritual Skull
-	questID = 51920, -- Cottontail Matron
 	vignetteID = 2332, -- Beastly Ritual Skull
 }
 
 NPCs[129950] = { -- Talon
 	achievementQuestID = 49528, -- Talon
 	isTameable = true,
-	questID = 51529, -- Talon
 }
 
 NPCs[129995] = { -- Emily Mayville
 	achievementQuestID = 49530, -- Emily Mayville
-	factionGroup = "Alliance",
-	questID = 51919, -- Emily Mayville
 }
 
 NPCs[130138] = { -- Nevermore
 	achievementQuestID = 49601, -- Nevermore
 	isTameable = true,
-	questID = 51908, -- Nevermore
 }
 
 NPCs[130143] = { -- Balethorn
 	achievementQuestID = 49602, -- Balethorn
-	questID = 51491, -- Balethorn
 }
 
 NPCs[132319] = { -- Bilefang Mother
 	achievementQuestID = 50163, -- Bilefang Mother
 	isTameable = true,
-	questID = 51512, -- Bilefang Mother
 }
 
 NPCs[134213] = { -- Executioner Blackwell
 	achievementQuestID = 50546, -- Executioner Blackwell
-	questID = 51527, -- Executioner Blackwell
 }
 
 NPCs[134706] = { -- Deathcap
 	achievementQuestID = 50669, -- Deathcap
-	questID = 51461, -- Deathcap
 }
 
 NPCs[134754] = { -- Hyo'gi
 	achievementQuestID = 50688, -- Hyo'gi
 	isTameable = true,
-	questID = 51467, -- Hyo'gi
 }
 
 NPCs[135796] = { -- Captain Leadfist
 	achievementQuestID = 50939, -- Captain Leadfist
-	questID = 51528, -- Captain Leadfist
 }
 
 NPCs[137529] = { -- Arvon the Betrayed
 	achievementQuestID = 51383, -- Arvon the Betrayed
-	questID = 51466, -- Arvon the Betrayed
 }
 
 NPCs[137665] = { -- Soul Goliath
-	questID = 51431, -- Soul Goliath
+	questID = 52002, -- Soul Goliath
 }
 
 NPCs[137704] = { -- Matron Morana
-	questID = 51433, -- Matron Morana
+	questID = 52000, -- Matron Morana
 }
 
 NPCs[137708] = { -- Stone Golem
-	questID = 51434, -- Stone Golem
+	questID = 51999, -- Stone Golem
 }
 
 NPCs[137824] = { -- Arclight
 	achievementQuestID = 51470, -- Arclight
 	isTameable = true,
-	questID = 51541, -- Arclight
 }
 
 NPCs[137825] = { -- Avalanche
 	achievementQuestID = 51471, -- Avalanche
 	isTameable = true,
-	questID = 51542, -- Avalanche
 }
 
 NPCs[138618] = { -- Haywire Golem
 	achievementQuestID = 51698, -- Haywire Golem
-	questID = 51884, -- Haywire Golem
 }
 
 NPCs[138667] = { -- Blighted Monstrosity
-	questID = 51699, -- Blighted Monstrosity
+	questID = 52001, -- Blighted Monstrosity
 }
 
 NPCs[138675] = { -- Gorged Boar
 	achievementQuestID = 51700, -- Gorged Boar
 	isTameable = true,
-	questID = 51874, -- Gorged Boar
 }
 
 NPCs[138863] = { -- Sister Martha
 	achievementQuestID = 51748, -- Sister Martha
-	questID = 51906, -- Sister Martha
 }
 
 NPCs[138866] = { -- Mack
 	achievementQuestID = 51749, -- Fungi Trio
-	questID = 51887, -- Fungi Trio
+	vignetteID = 2979, -- Fungi Trio
 }
 
 NPCs[138870] = { -- Mick
 	achievementQuestID = 51749, -- Fungi Trio
-	questID = 51887, -- Fungi Trio
+	vignetteID = 2979, -- Fungi Trio
 }
 
 NPCs[138871] = { -- Ernie
 	achievementQuestID = 51749, -- Fungi Trio
-	questID = 51887, -- Fungi Trio
+	vignetteID = 2979, -- Fungi Trio
 }
 
 NPCs[139321] = { -- Braedan Whitewall
 	achievementQuestID = 51922, -- Braedan Whitewall
-	questID = 51989, -- Braedan Whitewall
 }
 
 NPCs[139322] = { -- Whitney "Steelclaw" Ramsay
 	achievementQuestID = 51923, -- Hexed Hunter
-	questID = 51988, -- Whitney "Steelclaw" Ramsay
 	vignetteID = 3014, -- "Whitney ""Steelclaw"" Ramsay"
 }
 
 NPCs[139358] = { -- The Caterer
-	questID = 51970, -- The Caterer
+	questID = 51949, -- The Caterer
 	vignetteID = 3027, -- Ruined Wedding Cake
 }
 
@@ -232,17 +202,14 @@ NPCs[140252] = { -- Hailstone Construct
 -- ----------------------------------------------------------------------------
 NPCs[129803] = { -- Whiplash
 	achievementQuestID = 52296, -- Whiplash
-	questID = 52299, -- Whiplash
 }
 
 NPCs[130079] = { -- Wagga Snarltusk
 	achievementQuestID = 50819, -- Wagga Snarltusk
-	questID = 52300, -- Wagga Snarltusk
 }
 
 NPCs[130897] = { -- Captain Razorspine
 	achievementQuestID = 50170, -- Captain Razorspine
-	questID = 51782, -- Captain Razorspine
 }
 
 NPCs[131404] = { -- Foreman Scripps
@@ -253,36 +220,31 @@ NPCs[131404] = { -- Foreman Scripps
 			spellID = 260175 -- Goldenmane
 		}
 	},
-	questID = 51781, -- Foreman Scripps
 }
 
 NPCs[132007] = { -- Galestorm
 	achievementQuestID = 50075, -- Galestorm
-	questID = 51776, -- Galestorm
 }
 
 NPCs[132047] = { -- Reinforced Hullbreaker
-	questID = 51905, -- Reinforced Hullbreaker
+	questID = 53611, -- Reinforced Hullbreaker
 }
 
 NPCs[134147] = { -- Beehemoth
 	isTameable = true,
-	questID = 52330, -- Beehemoth
+	questID = 50541, -- Beehemoth
 }
 
 NPCs[134884] = { -- Ragna
 	achievementQuestID = 50725, -- Ragna
-	questID = 51774, -- Ragna
 }
 
 NPCs[134897] = { -- Dagrus the Scorned
 	achievementQuestID = 50731, -- Dagrus the Scorned
-	questID = 51777, -- Dagrus the Scorned
 }
 
 NPCs[135939] = { -- Vinespeaker Ratha
 	achievementQuestID = 50037, -- Vinespeaker Ratha
-	questID = 52301, -- Vinespeaker Ratha
 }
 
 NPCs[135947] = { -- Strange Mushroom Ring
@@ -291,12 +253,11 @@ NPCs[135947] = { -- Strange Mushroom Ring
 
 NPCs[136183] = { -- Crushtacean
 	achievementQuestID = 50731, -- Crushtacean
-	questID = 51901, -- Crushtacean
+	isTameable = true,
 }
 
 NPCs[136189] = { -- The Lichen King
 	achievementQuestID = 50974, -- The Lichen King
-	questID = 52476, -- The Lichen King
 }
 
 NPCs[137025] = { -- Broodmother
@@ -305,12 +266,11 @@ NPCs[137025] = { -- Broodmother
 }
 
 NPCs[137649] = { -- Pest Remover Mk. II
-	questID = 51806,  -- Pest Remover Mk. II
+	questID = 53612,  -- Pest Remover Mk. II
 }
 
 NPCs[138938] = { -- Seabreaker Skoloth
 	achievementQuestID = 51757, -- Seabreaker Skoloth
-	questID = 51759, -- Seabreaker Skoloth
 }
 
 NPCs[138963] = { -- Nestmother Acada
@@ -321,53 +281,54 @@ NPCs[138963] = { -- Nestmother Acada
 NPCs[139298] = { -- Pinku'shon
 	achievementQuestID = 51959, -- Pinku'shon
 	isTameable = true,
-	questID = 51886, -- Pinku'shon
 }
 
 NPCs[139319] = { -- Slickspill
 	achievementQuestID = 51958, -- Slickspill
-	questID = 51921, -- Slickspill
 }
 
 NPCs[139328] = { -- Sabertron (Red Sabertron)
+	achievementQuestID = 51956,
 	isTameable = true,
 }
 
 NPCs[139335] = { -- Sabertron (Yellow Sabertron)
+	achievementQuestID = 51956,
 	isTameable = true,
 }
 
 NPCs[139336] = { -- Sabertron (Green Sabertron)
+	achievementQuestID = 51956,
 	isTameable = true,
 }
 
 NPCs[139356] = { -- Sabertron (Orange Sabertron)
+	achievementQuestID = 51956,
 	isTameable = true,
 }
 
 NPCs[139359] = { -- Sabertron (Copper Sabertron)
+	achievementQuestID = 51956,
 	isTameable = true,
 }
 
 NPCs[139385] = { -- Deepfang
 	achievementQuestID = 50692, -- Deepfang
 	isTameable = true,
-	questID = 51778, -- Deepfang
 }
 
 NPCs[139968] = { -- Corrupted Tideskipper
-	questID = 52310, -- Corrupted Tideskipper
+	achievementQuestID = 52121, -- Corrupted Pod
+	vignetteID = 3041, -- Corrupted Pod
 }
 
 NPCs[139980] = { -- Taja the Tidehowler
 	achievementQuestID = 52123, -- Taja the Tidehowler
-	questID = 52322, -- Taja the Tidehowler
 }
 
 NPCs[139988] = { -- Sandfang
 	achievementQuestID = 52125, -- Sandfang
 	isTameable = true,
-	questID = 52309, -- Sandfang
 }
 
 NPCs[140163] = { -- Warbringer Yenajz
@@ -375,7 +336,7 @@ NPCs[140163] = { -- Warbringer Yenajz
 }
 
 NPCs[140398] = { -- Zeritarj
-	questID = 52352, -- Zeritarj
+	questID = 53624, -- Zeritarj
 }
 
 NPCs[140925] = { -- Doc Marrtens
@@ -385,23 +346,19 @@ NPCs[140925] = { -- Doc Marrtens
 NPCs[140938] = { -- Croaker
 	achievementQuestID = 52303, -- Croaker
 	isTameable = true,
-	questID = 52306, -- Croaker
 }
 
 NPCs[140997] = { -- Severus the Outcast
 	achievementQuestID = 50938, -- Severus the Outcast
-	questID = 52315, -- Severus the Outcast
 }
 
 NPCs[141029] = { -- Kickers
 	achievementQuestID = 52318, -- Kickers
 	isTameable = true,
-	questID = 52321, -- Kickers
 }
 
 NPCs[141039] = { -- Ice Sickle
 	achievementQuestID = 52327, -- Ice Sickle
-	questID = 52328, -- Ice Sickle
 }
 
 NPCs[141043] = { -- Jakala the Cruel
@@ -410,33 +367,27 @@ NPCs[141043] = { -- Jakala the Cruel
 
 NPCs[141059] = { -- Grimscowl the Harebrained
 	achievementQuestID = 52329, -- Grimscowl the Harebrained
-	questID = 51779, -- Rage Paw
 }
 
 NPCs[141088] = { -- Squall
 	achievementQuestID = 52433, -- Squall
-	questID = 52432, -- Unrelenting Squall
 }
 
 NPCs[141143] = { -- Sister Absinthe
 	achievementQuestID = 52441, -- Sister Absinthe
-	questID = 52446, -- Tidesage Absinthe
 }
 
 NPCs[141175] = { -- Song Mistress Dadalea
 	achievementQuestID = 52448, -- Song Mistress Dadalea
-	questID = 52452, -- Song Mistress Dadalea
 }
 
 NPCs[141226] = { -- Haegol the Hammer
 	achievementQuestID = 52460, -- Haegol the Hammer
-	questID = 52463, -- Haegol the Hammer
 }
 
 NPCs[141239] = { -- Osca the Bloodied
 	achievementQuestID = 52461, -- Osca the Bloodied
 	isTameable = true,
-	questID = 52464, -- Osca the Bloodied
 }
 
 NPCs[141286] = { -- Poacher Zane
@@ -446,24 +397,25 @@ NPCs[141286] = { -- Poacher Zane
 NPCs[142088] = { -- Whirlwing
 	achievementQuestID = 52457, -- Whirlwing
 	isTameable = true,
-	questID = 52459, -- Whirlwing
 }
 
 -- ----------------------------------------------------------------------------
 -- Tiragarde Sound
 -- ----------------------------------------------------------------------------
 NPCs[127289] = { -- Saurolisk Tamer Mugg
-	questID = 51656, -- Saurolisk Tamer Mugg
+	questID = 48806, -- Saurolisk Tamer Mugg
+	vignetteID = 2263, -- Saurolisk Tamer Mugg
 }
 
 NPCs[127290] = { -- Mugg
 	isTameable = true,
-	questID = 51656, -- Saurolisk Tamer Mugg
+	questID = 48806, -- Saurolisk Tamer Mugg
+	vignetteID = 2263, -- Saurolisk Tamer Mugg
 }
 
 NPCs[129181] = { -- Barman Bill
 	achievementItemID = 163717, -- Forbidden Sea Shanty of Josephus
-	questID = 51652, -- Barman Bill
+	questID = 50233, -- Barman Bill
 }
 
 NPCs[130508] = { -- Broodmother Razora
@@ -473,12 +425,12 @@ NPCs[130508] = { -- Broodmother Razora
 
 NPCs[131252] = { -- Merianae
 	isTameable = true,
-	questID = 51659, -- Merianae
+	questID = 49921,
 }
 
 NPCs[132086] = { -- Black-Eyed Bart
 	achievementItemID = 163718, -- Forbidden Sea Shanty of the Black Sphere
-	questID = 51669, -- Black-Eyed Bart
+	questID = 50096,
 
 }
 
