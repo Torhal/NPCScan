bb5c0e521640a2546a197166b7a7055c3ebab13d
James D. Callahan III <darkenelf@gmail.com>
2026-04-13 14:01:54 -0400

    Move NPCData annotations to their own file.

---
+++ Annotations/NPCData.d.lua
@@ -0,0 +1,34 @@
+---@meta _
+
+--------------------------------------------------------------------------------
+---- Types
+--------------------------------------------------------------------------------
+
+---@class MountData
+---@field itemID integer
+---@field spellID integer
+
+---@class PetData
+---@field itemID integer
+---@field npcID integer
+
+---@class ToyData
+---@field itemID integer
+
+---@class NPCData
+---@field achievementAssetName? string
+---@field achievementCriteriaID? integer
+---@field achievementID? integer
+---@field achievementQuestID? integer
+---@field classification? NPCClassification
+---@field factionGroup? "Alliance" | "Horde"
+---@field isCriteriaCompleted? boolean
+---@field isTameable? boolean
+---@field mapIDs? integer[]
+---@field mounts? MountData[]
+---@field npcID integer
+---@field pets? PetData[]
+---@field questID? integer
+---@field toys? ToyData[]
+---@field vignetteID? integer
+---@field worldQuestID? integer
