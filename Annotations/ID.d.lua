ea51ccda78c1579aae208d3440755089adbd03c0
James D. Callahan III <darkenelf@gmail.com>
2026-04-25 10:46:10 -0400

    Add type aliases for the various IDs; achievementID, mapID, etc.

---
+++ Annotations/ID.d.lua
@@ -0,0 +1,13 @@
+---@meta _
+
+--------------------------------------------------------------------------------
+---- Types
+--------------------------------------------------------------------------------
+
+---@alias AchievementID integer
+---@alias ContinentID integer
+---@alias ItemID integer
+---@alias MapID integer
+---@alias NPCID integer
+---@alias SpellID integer
+---@alias QuestID integer
