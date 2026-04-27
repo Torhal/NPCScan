4eb88429d8986349339599b25d6a10a47687434e
James D. Callahan III <darkenelf@gmail.com>
2026-04-26 21:55:04 -0400

    Add AchievementData type.

---
+++ Annotations/AchievementData.d.lua
@@ -0,0 +1,13 @@
+---@meta _
+
+--------------------------------------------------------------------------------
+---- Types
+--------------------------------------------------------------------------------
+
+---@class AchievementData
+---@field ID AchievementID
+---@field criteriaNPCs table --- TODO: FIX
+---@field description string
+---@field iconTexturePath string | integer
+---@field isCompleted boolean
+---@field name string
