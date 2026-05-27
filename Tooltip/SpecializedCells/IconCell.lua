2ac6f52fde442a0afd61266dfd2b2623ad08bbdc
James D. Callahan III <darkenelf@gmail.com>
2026-05-27 10:09:49 -0400

    IconCell: Formatting changes.

--- Tooltip/SpecializedCells/IconCell.lua
+++ Tooltip/SpecializedCells/IconCell.lua
@@ -25,42 +25,42 @@ local DefaultIconSize = 18
 --------------------------------------------------------------------------------
 
 function IconCell:GetContentHeight()
-	return self.IconTexture:GetHeight()
+    return self.IconTexture:GetHeight()
 end
 
 -- Returns the size of the IconCell.
 ---@return number width The width of the IconCell.
 ---@return number height The height of the IconCell.
 function IconCell:GetSize()
-	return self.IconTexture:GetSize()
+    return self.IconTexture:GetSize()
 end
 
 function IconCell:OnCreation()
-	BaseCell.OnCreation(self)
+    BaseCell.OnCreation(self)
 
-	local iconTexture = self:CreateTexture(nil, "ARTWORK")
-	iconTexture:SetSize(DefaultIconSize, DefaultIconSize)
-	iconTexture:SetPoint("CENTER", self)
+    local iconTexture = self:CreateTexture(nil, "ARTWORK")
+    iconTexture:SetSize(DefaultIconSize, DefaultIconSize)
+    iconTexture:SetPoint("CENTER", self)
 
-	self.IconTexture = iconTexture
+    self.IconTexture = iconTexture
 end
 
 function IconCell:OnRelease()
-	BaseCell.OnRelease(self)
+    BaseCell.OnRelease(self)
 
-	self.IconTexture:SetSize(DefaultIconSize, DefaultIconSize)
-	self.IconTexture:SetTexture(nil)
+    self.IconTexture:SetSize(DefaultIconSize, DefaultIconSize)
+    self.IconTexture:SetTexture(nil)
 end
 
 ---@param width number
 ---@param height number
 ---@return LibQTip-2.0.IconCell
 function IconCell:SetIconSize(width, height)
-	self.IconTexture:SetSize(width, height)
+    self.IconTexture:SetSize(width, height)
 
-	self:OnContentChanged()
+    self:OnContentChanged()
 
-	return self
+    return self
 end
 
 -- Works identically to the default UI's texture:SetTexCoord() API, for the IconCell's IconTexture.
@@ -69,18 +69,18 @@ end
 ---@overload fun(minX: number, maxX: number, minY: number, maxY: number)
 ---@return LibQTip-2.0.IconCell
 function IconCell:SetIconTexCoord(...)
-	self.IconTexture:SetTexCoord(...)
+    self.IconTexture:SetTexCoord(...)
 
-	return self
+    return self
 end
 
 --- Populates the Cell with an icon texture.
 ---@param texturePath number|string The path to the icon asset.
 ---@return LibQTip-2.0.IconCell
 function IconCell:SetIconTexture(texturePath)
-	self.IconTexture:SetTexture(texturePath)
+    self.IconTexture:SetTexture(texturePath)
 
-	self:OnContentChanged()
+    self:OnContentChanged()
 
-	return self
+    return self
 end
